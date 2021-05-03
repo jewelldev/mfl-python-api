6e9c9329eacb40ef6458fa8455eaab04e5096a9e
LIBRESTREAM\brett <bjewelldev@gmail.com>
2021-05-02 20:27:23 -0500

    WIP

--- session.py
+++ session.py
@@ -1,13 +1,52 @@
+'''
+session.py
+
+MyFantasyLeagueAPISession object
+
+'''
+
 from mfl_request import MFLRequest, MFLLoginRequest, MFLRostersRequest, MFLPlayersRequest, MFLLeagueRequest, MFLLiveScoringRequest, MFLPlayerScoresRequest
 from mfl_response import MFLResponse
+import json
 
 class MyFantasyLeagueAPISession():
+    """The summary line for a class docstring should fit on one line.
 
+    If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. Alternatively, attributes may be documented
+    inline with the attribute's declaration (see __init__ method below).
+
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
+
+    Attributes:
+        attr1 (str): Description of `attr1`.
+        attr2 (:obj:`int`, optional): Description of `attr2`.
+
+    """
     host = "www67.myfantasyleague.com"
     protocol = "https"
 
     def __init__(self, year, league_id="", username="", password=""):
-        """"Initializes a new MyFantasyLeague API session"""
+        """Example of docstring on the __init__ method.
+
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+
+        Either form is acceptable, but the two should not be mixed. Choose one
+        convention to document the __init__ method and be consistent with it.
+
+        Note:
+            Do not include the `self` parameter in the ``Args`` section.
+
+        Args:
+            param1 (str): Description of `param1`.
+            param2 (:obj:`int`, optional): Description of `param2`. Multiple
+                lines are supported.
+            param3 (:obj:`list` of :obj:`str`): Description of `param3`.
+
+        """
         self.year = year
         self.league_id = league_id
         self.username = username
@@ -77,6 +116,7 @@ class MyFantasyLeagueAPISession():
             MFLPlayersResponse
         """
         league_id = league_id if league_id is not None else self.league_id
+        
         request = MFLPlayersRequest(league_id, details, since, players)
         response = request.make_request()
         return response
@@ -96,6 +136,7 @@ class MyFantasyLeagueAPISession():
         
         """
         league_id = league_id if league_id is not None else self.league_id
+        
         request = MFLLeagueRequest(league_id)
         response = request.make_request()
         return response     
